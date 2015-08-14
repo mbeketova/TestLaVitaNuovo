cb2464292f9d87054f70d6ba532d84a8032dea53
Mariya <mbeketova@rambler.ru>
2015-08-14 18:42:56 +0300

    Initial Commit

---
+++ TestLaVitaNuovo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLaVitaNuovo
+//
+//  Created by Admin on 14.08.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLaVitaNuovo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLaVitaNuovo
+//
+//  Created by Admin on 14.08.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
